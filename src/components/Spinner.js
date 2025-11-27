684d6a12f6c01eb618b3c886a729d3f9a4408ddd
bluenote28 <136389327+bluenote28@users.noreply.github.com>
2025-11-27 05:00:44 -0600

    changes color to white

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -3,7 +3,7 @@ import Spinner from 'react-bootstrap/Spinner';
 function NormalSpinner() {
   return (
      <div className='d-flex justify-content-center align-items-center h-100'>
-        <Spinner animation="border" role="status">
+        <Spinner animation="border" role="status" variant='light'>
             <span className="visually-hidden">Loading...</span>
         </Spinner>
     </div>
