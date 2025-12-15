89e0aa38a79186d2fb7dbce5b2b8bf40eaa90b74
anthonycowden <acowden@wgu.edu>
2025-12-15 07:16:28 -0600

    redoes pagination bar to use first,last,next,prev buttons

--- src/components/DisplayListings.js
+++ src/components/DisplayListings.js
@@ -8,6 +8,7 @@ import ListingFilter from '../utilities/FilterClass';
 import formatItemsIntoRows from '../utilities/ItemsGridFormatter';
 import NormalSpinner from './Spinner';
 import Pagination from 'react-bootstrap/Pagination';
+import { Container } from 'react-bootstrap';
 
 function DisplayListings(props) {
 
@@ -17,7 +18,6 @@ function DisplayListings(props) {
   const [ filteredItems, setFilteredItems ] = useState([])
   const [filteringItems, setFilteringItems ] = useState(true)
   const [page, setPage ] = useState(1)
-  let paginationItems = [];
   const ITEMS_PER_PAGE = 48;
 
   useEffect(() => {
@@ -51,19 +51,37 @@ function DisplayListings(props) {
   else{
 
       const totalPages = Math.ceil(filteredItems[0].length / ITEMS_PER_PAGE);
-
-      for (let number = 1; number <= totalPages; number++) {
-          paginationItems.push(<Pagination.Item key={number} active={number === page} onClick={() => setPage(number)}>{number}</Pagination.Item>);
-      }
-
       const paginationItemsEnding = ITEMS_PER_PAGE * page;
       const paginationItemsBeginning = paginationItemsEnding - ITEMS_PER_PAGE;
       const paginatedItems = formatItemsIntoRows(filteredItems[0].slice(paginationItemsBeginning, paginationItemsEnding));
+      const prevPaginationItems = [<Pagination.First onClick={() => setPage(1)} />, <Pagination.Prev onClick={()=>{
+
+        if(page == 1){
+          return;
+        }
+        else{
+          setPage(page - 1);
+        }
+
+      }
+      } />];
+      const nextPaginationItems = [<Pagination.Next onClick={()=>{
+
+        if(page == totalPages){
+          return;
+        }
+        else{
+          setPage(page + 1);
+        }
+
+      }
+      }  />, <Pagination.Last onClick={() => setPage(totalPages)} />];
   
       return (
         <>
-
-        <Pagination>{paginationItems}</Pagination>
+        <Container className='d-flex justify-content-center'>
+            <Pagination>{prevPaginationItems}<Pagination.Item>{page}</Pagination.Item><Pagination.Item>of</Pagination.Item><Pagination.Item>{totalPages}</Pagination.Item>{nextPaginationItems}</Pagination>
+        </Container>
             {   
                 paginatedItems.map((item, index) => { 
                     
@@ -89,7 +107,9 @@ function DisplayListings(props) {
                 })
             }
 
-        <Pagination>{paginationItems}</Pagination>
+        <Container className='d-flex justify-content-center'>
+            <Pagination>{prevPaginationItems}<Pagination.Item>{page}</Pagination.Item><Pagination.Item>of</Pagination.Item><Pagination.Item>{totalPages}</Pagination.Item>{nextPaginationItems}</Pagination>
+        </Container>
             
         </>  
         )
