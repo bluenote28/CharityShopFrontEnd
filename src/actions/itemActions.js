687decfa3082b27dc139b32e7bdd12af0661bccb
anthonycowden <acowden@wgu.edu>
2025-12-23 03:04:11 -0600

    refactors to not use items api

--- src/actions/itemActions.js
+++ src/actions/itemActions.js
@@ -1,12 +1,17 @@
 import { GET_ITEMS_REQUEST, GET_ITEMS_ERROR, GET_ITEMS_SUCCESS } from "../constants/reducerConstants"
-import { ItemsApi } from "../utilities/ApiClient"
+import { BACKEND_API_BASE_URL } from "../constants/apiContants"
 
-const itemsApi = new ItemsApi()
-
-export const getItems = () => async(dispatch) => {
+export const getItems = () => async(dispatch, getState) => {
     try{
-        dispatch({type: GET_ITEMS_REQUEST})
-        const data = await itemsApi.getAllData()
+        dispatch({type: GET_ITEMS_REQUEST})          
+        const config = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+          }
+        const response = await fetch(BACKEND_API_BASE_URL + 'items/ebaycharityitems/', config)
+        const data = await response.json()
         dispatch({type: GET_ITEMS_SUCCESS, payload: data})
     }catch(error){
         dispatch({type: GET_ITEMS_ERROR, error: error})
