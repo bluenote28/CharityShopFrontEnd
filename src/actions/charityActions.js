9394838098082bc27ad041973a18b3c325f14fd8
anthonycowden <acowden@wgu.edu>
2025-12-23 07:06:14 -0600

    adds delete and add charity actions

--- src/actions/charityActions.js
+++ src/actions/charityActions.js
@@ -1,10 +1,9 @@
 import { BACKEND_API_BASE_URL } from '../constants/apiContants'
-import { GET_CHARITIES_REQUEST, GET_CHARITIES_ERROR, GET_CHARITIES_SUCCESS } from '../constants/reducerConstants'
+import { GET_CHARITIES_REQUEST, GET_CHARITIES_ERROR, GET_CHARITIES_SUCCESS, DELETE_CHARITY_ERROR, DELETE_CHARITY_REQUEST, DELETE_CHARITY_SUCCESS, ADD_CHARITY_ERROR, ADD_CHARITY_REQUEST, ADD_CHARITY_SUCCESS } from '../constants/reducerConstants'
 
-export const getCharities = () => async(dispatch, getState) => {
+export const getCharities = () => async(dispatch) => {
     try{
         dispatch({type: GET_CHARITIES_REQUEST})
-        const { userLogin: { userInfo } } = getState()
         
         const config = {
             method: 'GET',
@@ -17,5 +16,51 @@ export const getCharities = () => async(dispatch, getState) => {
         dispatch({type: GET_CHARITIES_SUCCESS, payload: data})
     }catch(error){
         dispatch({type: GET_CHARITIES_ERROR, error: error})
+    }
+}
+
+export const deleteCharity = (id) => async(dispatch, getState) => {
+    try{
+        dispatch({type: DELETE_CHARITY_REQUEST})
+        const { userLogin: { userInfo } } = getState()
+        
+        const config = {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            }
+        }
+         const response = await fetch(BACKEND_API_BASE_URL + 'charity/deleteCharity/' + id, config)
+         const data = await response.json()
+         console.log(data)
+        dispatch({type: DELETE_CHARITY_SUCCESS, payload: data})
+    }catch(error){
+        dispatch({type: DELETE_CHARITY_ERROR, error: error})
+    }
+}
+
+export const addCharity = (charity) => async(dispatch, getState) => {
+    try{
+        dispatch({type: ADD_CHARITY_REQUEST})
+        const { userLogin: { userInfo } } = getState()
+        
+        const config = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${userInfo.token}`
+            },
+            body: JSON.stringify(charity)
+        }
+         const response = await fetch(BACKEND_API_BASE_URL + 'charity/addCharity/', config)
+         const data = await response.json()
+
+         console.log(data)
+         console.log(response)
+         console.log(config.data)
+        dispatch({type: ADD_CHARITY_SUCCESS, payload: data})
+    }catch(error){
+        dispatch({type: ADD_CHARITY_ERROR, error: error})
     }
 }
