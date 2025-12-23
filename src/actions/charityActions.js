2e2e4d59c61c63aa58ffa4427c704ec6a30d5b09
anthonycowden <acowden@wgu.edu>
2025-12-23 03:03:39 -0600

    refactors to not use charity api

--- src/actions/charityActions.js
+++ src/actions/charityActions.js
@@ -1,12 +1,19 @@
+import { BACKEND_API_BASE_URL } from '../constants/apiContants'
 import { GET_CHARITIES_REQUEST, GET_CHARITIES_ERROR, GET_CHARITIES_SUCCESS } from '../constants/reducerConstants'
-import { CharityApi } from "../utilities/ApiClient"
 
-const charityApi = new CharityApi()
-
-export const getCharities = () => async(dispatch) => {
+export const getCharities = () => async(dispatch, getState) => {
     try{
         dispatch({type: GET_CHARITIES_REQUEST})
-        const data = await charityApi.getAllData()
+        const { userLogin: { userInfo } } = getState()
+        
+        const config = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+         const response = await fetch(BACKEND_API_BASE_URL + 'charity/getCharities', config)
+         const data = await response.json()
         dispatch({type: GET_CHARITIES_SUCCESS, payload: data})
     }catch(error){
         dispatch({type: GET_CHARITIES_ERROR, error: error})
