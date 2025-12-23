8112f624fb0e0a5cd447e51bee701c9022ab54c8
anthonycowden <acowden@wgu.edu>
2025-12-23 07:08:36 -0600

    adds add and delete charity reducers

--- src/reducers/charityReducers.js
+++ src/reducers/charityReducers.js
@@ -1,4 +1,4 @@
-import { GET_CHARITIES_REQUEST, GET_CHARITIES_ERROR, GET_CHARITIES_SUCCESS } from '../constants/reducerConstants'
+import { GET_CHARITIES_REQUEST, GET_CHARITIES_ERROR, GET_CHARITIES_SUCCESS, DELETE_CHARITY_REQUEST, DELETE_CHARITY_ERROR, DELETE_CHARITY_SUCCESS, ADD_CHARITY_ERROR, ADD_CHARITY_REQUEST, ADD_CHARITY_SUCCESS } from '../constants/reducerConstants'
 
 export const charityReducer = (state = {charities:[]}, action) => {
 
@@ -9,6 +9,18 @@ export const charityReducer = (state = {charities:[]}, action) => {
                 return {loading: false, charities: action.payload}
             case GET_CHARITIES_ERROR:
                 return {loading: false, error: action.error, charities: []}
+            case DELETE_CHARITY_REQUEST:
+                return {loading: true, charities: []}
+            case DELETE_CHARITY_ERROR:
+                return {loading: false, error: action.error, charities: []}
+            case DELETE_CHARITY_SUCCESS:
+                return {loading: false, charities: action.payload}
+            case ADD_CHARITY_REQUEST:
+                return {loading: true, charities: []}
+            case ADD_CHARITY_ERROR:
+                return {loading: false, error: action.error, charities: []}
+            case ADD_CHARITY_SUCCESS:
+                return {loading: false, charities: action.payload}
             default:
                 return state
         }
