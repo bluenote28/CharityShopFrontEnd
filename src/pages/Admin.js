840514156681a2efb716a3f8b2568c610e825715
anthonycowden <acowden@wgu.edu>
2026-01-03 09:47:31 -0600

    removes uneeded containers

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -3,7 +3,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getCharities } from '../actions/charityActions';
 import CharitiesTable from '../components/CharitiesTable';
 import SubmitCharityForm from '../components/SubmitCharityForm';
-import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import AdminReport from '../components/AdminReport';
@@ -30,25 +29,19 @@ function AdminPage() {
   else {
     return (
         <Col>
-                <Row className='w-50 mt-3 m-auto border rounded-3'>
-                    <Container>
+                <Row className='mt-3 border rounded-3 mx-5 px-3'>
                         <h4 className='mt-2' style={{textAlign: 'center'}}> Charities Database</h4>
-                        <CharitiesTable data={charities} />
-                    </Container>
+                        <CharitiesTable data={charities} />     
                 </Row>
 
-
-                <Row className='w-50 mt-3 m-auto border rounded-3'>
-                    <Container>
+                <Row className='w-50 mt-3 m-auto border rounded-3 px-3'>
                         <h4 className='mt-2' style={{textAlign: 'center'}}>Enter or Update a Charity</h4>
                         <SubmitCharityForm />
-                    </Container>
                 </Row>
-                <Row className='w-50 mt-3 mb-5 m-auto border rounded-3'>
-                    <Container>
+
+                <Row className='w-50 mt-3 mb-5 m-auto border rounded-3 px-3'>
                         <h4 className='mt-2' style={{textAlign: 'center'}}>Database Report</h4>
                         <AdminReport />
-                    </Container>
                 </Row>
         </Col>
     )
