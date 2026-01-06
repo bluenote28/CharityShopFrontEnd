22e39f18cb9e41987aacdfa20cf03c7c26818244
anthonycowden <acowden@wgu.edu>
2026-01-06 00:38:44 -0600

    implements subCategory filter

--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -8,7 +8,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import AlertBox from '../components/Alert'
 import { useSearchParams } from 'react-router-dom';
 import { getCharities } from '../actions/charityActions';
-import SearchBar from '../components/SearchBar';
+import { SUB_CATEGORY_OPTIONS } from '../constants/categoryFilterOptions'
 
 function CategoryPage() {
 
@@ -17,6 +17,7 @@ function CategoryPage() {
   const [errorMessage, setErrorMessage ] = useState(null)
   const [searchParams, setSearchParams] = useSearchParams();
   const category = searchParams.get('category')
+  const [subCategory, setSubCategory] = useState(null)
   const dispatch = useDispatch();
   const charitiesState = useSelector((state) => state.charities);
   const { error, loading, charities} = charitiesState;
@@ -29,40 +30,49 @@ function CategoryPage() {
 
   useEffect(() => {
     
-          if (charities){
-                  
-              let select_options = [{value: null, label: "All Charities"}];
+    if (charities){
+            
+        let select_options = [{value: null, label: "All Charities"}];
 
-              for (let i = 0; i < charities.length; i++) {
-                select_options.push({ value: charities[i].id, label: charities[i].name })
-              }              
-              setAllCharitites(select_options);
-            }
-            else{
-              setErrorMessage("Error retrieving charities from server")
-            }
+        for (let i = 0; i < charities.length; i++) {
+          select_options.push({ value: charities[i].id, label: charities[i].name })
+        }              
+        setAllCharitites(select_options);
+      }
+      else{
+        setErrorMessage("Error retrieving charities from server")
+      }
   }, [charities])
 
 
   return (
     <>
-    {errorMessage && <AlertBox message={errorMessage}/>}
-  
-      <Row>
-        <Col>
-            <Container className='mt-4 mb-1'>
-                <SearchBar />
-            </Container>
-            
-            <Container className='d-flex justify-content-start mb-3 mt-1'>
-                    
-              <Select className="mx-1 w-25" options={allCharitites} onChange={(e) => setCharity(e.value)} defaultValue={{value: null, label: "All Charities"}} /> 
-                     
-            </Container>
-        </Col>
-      </Row>
+    {errorMessage && <AlertBox message={errorMessage}/>}.             
+    
+      <Container>
+        <Row>
+            <Col>
+                <h1 style={{textAlign:"center"}}>{category}</h1>
+            </Col>
+        </Row>
+      </Container>
 
-      <DisplayListings charityId={charity} category={category}/>
+      <Container className='d-flex justify-content-center mb-3 mt-1'>
+        <Row>
+            <Col>            
+            <Select className="mx-1 w-100" options={allCharitites} onChange={(e) => setCharity(e.value)} defaultValue={{value: null, label: "All Charities"}} /> 
+            </Col> 
+            <Col>
+            <Select className="mx-1 w-100" options={SUB_CATEGORY_OPTIONS[category]} onChange={(e) => setSubCategory(e.value)} defaultValue={{value: null, label: "All Categories"}} />
+            </Col>           
+        </Row>
+      </Container>
+      
+      <Container>
+        <Row>
+            <Col><DisplayListings charityId={charity} category={category} subCategory={subCategory} /></Col>
+        </Row>
+      </Container>
        
       </> 
   )
