9d06e99f9487e4d34350b3787fdcea5780bbb384
anthonycowden <acowden@wgu.edu>
2026-01-06 19:01:37 -0600

    adds more subcategories

--- src/constants/categoryFilterOptions.js
+++ src/constants/categoryFilterOptions.js
@@ -22,16 +22,98 @@ export const CATEGORY_OPTIONS = [
 ]
 
 export const SUB_CATEGORY_OPTIONS = {
-        "Collectibles": [{value: "Trading Card Singles", label: "Trading Card Singles"},
+        "Collectibles": [{ value: null, label: "All Collectibles"},
+                        { value: "Trading Card Singles", label: "Trading Card Singles"},
+                        { value: "Trading Card Lots", label: "Trading Card Lots"},
+                        { value: "Comics", label: "Comics"},
+                        { value: "Collectible Figures & Bobbleheads", label: "Collectible Figures & Bobbleheads"},
+                        { value: "Coca-Cola", label: "Coca-Cola"},
+                        { value: "Disney", label: "Disney"},
                         { value: "Postcards", label: "Postcards"},
                         { value: "Coins & Paper Money", label: "Coins & Paper Money"},
                         { value: "Stamps", label: "Stamps"},
                         { value: "Sports Mem, Cards & Fan Shop", label: "Sports Mem, Cards & Fan Shop"}, 
-                        {value: "Antiques", label: 'Antiques'},
-                        { value: "Advertising", label: "Advertising"}],
+                        { value: "Antique (Pre-1900)", label: 'Antique (Pre-1900)'},
+                        { value: "Civil War (1861-65)", label: "Civil War (1861-65)"},
+                        { value: "Advertising", label: "Advertising"},
+                        { value: "Records", label: "Records"},
+                        { value: "Star Wars Collectibles", label: "Star Wars Collectibles"},
+                        { value: "Patches, Pins & Buttons", label: "Patches, Pins & Buttons"}],
         "Clothing, Shoes & Accessories": [{value: null, label: "All Clothing"},       
                                          {value: "Women's Clothing", label: "Womens's Clothing"},
+                                         {value: "Women's Bags & Handbags", label: "Women's Bags & Handbags"},
                                          {value: "Girls", label: "Girls"}, 
                                          {value: "Boys", label: "Boys"},
-                                         {value:"Men's Clothing", label: "Men's Clothing"}]
+                                         {value: "Men", label: "Men"},
+                                         {value:"Men's Clothing", label: "Men's Clothing"},
+                                        {value: "Baby & Toddler Clothing", label:"Baby & Toddler Clothing"}],
+
+        "Books & Magazines": [{value: null, label: "All Books & Magazines"},
+                              {value: "Textbooks", label: "Textbooks"},
+                              {value: "Magazines", label: "Magazines"},
+                              {value: "Books", label: "Books"},
+                              {value: "Antiquarian & Collectible", label: "Antiquarian & Collectible"},
+                              {value: "Audiobooks", label: "Audiobooks"},
+                              {value: "Study Guides & Test Prep", label: "Study Guides & Test Prep"},
+                              {value: "Dictionaries & Reference", label: "Dictionaries & Reference"}],
+
+        "Video Games & Consoles": [{value: null, label: "All Video Games & Consoles"},
+                                    {value: "Video Game Consoles", label: "Video Game Consoles"},
+                                    {value: "Video Games", label: "Video Games"},
+                                    {value: "Controllers & Attachments", label: "Controllers & Attachments"},
+                                    {value: "Video Game Merchandise", label: "Video Game Merchandise"},
+                                    {value: "Headsets", label: "Headsets"},
+                                    {value: "Original Game Cases & Boxes", label: "Original Game Cases & Boxes"}],
+
+        "Jewelry & Watches": [{value: null, label: "All Jewelery & Watches"},
+                                {value: "Rings", label: "Rings"},
+                                {value: "Watches", label: "Watches"},
+                                {value: "Earrings", label: "Earrings"},
+                                {value: "Bracelets & Charms", label: "Bracelets & Charms"}
+        ],
+
+        "Movies & TV": [{value: null, label: "All Movies & TV"},
+                        { value: "DVDs & Blu-ray Discs", label: "DVDs & Blu-ray Discs" },
+                        { value: "VHS Tapes", label: "VHS Tapes"},
+                        { value: "Film Stock", label: "Film Stock"},
+                        { value: "Other Formats", label: "Other Formats"}
+        ],
+
+        "Toys & Hobbies": [{value: null, label: "All Toys & Hobbies"},
+                         {value: "Action Figures", label: "Action Figures"},
+                         {value: "Fisher Price", label: "Fisher Price"},
+                         {value: "Disney", label: "Disney"},
+                         {value: "LEGO (R) Complete Sets & Packs", label: "LEGO (R) Complete Sets & Packs"},
+                         {value: "LEGO (R) Building Toys", label: "LEGO (R) Building Toys"},
+                         {value: "LEGO (R) Bricks, Pieces & Parts", label: "LEGO (R) Bricks, Pieces & Parts"},
+                         {value: "Puzzles", label: "Puzzles"},
+                         {value: "Electronic Games", label: "Electronic Games"},
+                         {value: "Models & Kits", label: "Models & Kits"},
+                         {value: "Cars: Racing, NASCAR", label: "Cars: Racing, NASCAR"},
+                         {value: "Star Wars", label: "Star Wars"}
+        ],
+
+        "Crafts": [{value: null, label: "All Crafts"},
+                   {value: "Sewing", label: "Sewing"},
+                   {value: "Quilting", label: "Quilting"},
+                   {value: "Embroidery & Cross Stitch", label: "Embroidery & Cross Stitch"},
+                   {value: "Kids' Crafts", label: "Kids' Crafts"},
+                   {value: "Craft Kits", label: "Craft Kits"}
+        ],
+
+        "Sporting Goods": [{value: null, label: "All Sporting Goods"},
+                           {value: "Football", label: "Football"},
+                           {value: "Basketball", label: "Basketball"},
+                           {value: "Baseball", label: "Baseball"},
+                           {value: "Golf", label: "Golf"},
+                           {value: "Soccer", label: "Soccer"},
+                           {value: "Tennis", label: "Tennis"},
+                           {value: "Cycling", label: "Cycling"},
+                           {value: "Skateboarding & Longboarding", label: "Skateboarding & Longboarding"},
+                           {value: "Fishing", label: "Fishing"},
+                           {value: "Hunting", label: "Hunting"},
+                           {value: "Ice Skating", label: 'Ice Skating'}
+        ]
+        
+        
 }
