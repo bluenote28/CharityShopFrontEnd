26ab98e43336ed778aeec72b68cf21c0b3a8cf38
Cowden <cowdena@amazon.com>
2026-01-08 18:03:06 -0600

    adds cursor style

--- src/components/CategoryIcon.js
+++ src/components/CategoryIcon.js
@@ -1,13 +1,14 @@
 import Col from 'react-bootstrap/Col'
 import Image from 'react-bootstrap/Image';
 
-
 function CategoryIcon(props) {
 
-  return (
-    <Col>
-      <Image src={props.src} fluid rounded onClick={props.onclick} />
-    </Col>
+return (
+  <Col>
+      <Image src={props.src} fluid rounded 
+        onClick={props.onclick}
+        style={{ cursor: 'pointer' }} />
+  </Col>
   );
 }
 
