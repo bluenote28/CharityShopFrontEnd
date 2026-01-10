c2a87326a6384aed73cae9fa331fd2830c204816
Cowden <cowdena@amazon.com>
2026-01-09 18:42:05 -0600

    styling and text changes

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,20 +2,25 @@ import { Container } from "react-bootstrap";
 
 function AboutPage() {
 
+    const containerStyle = {
+        fontFamily: "arial", 
+    }
+
     return (
         <>
-           <h1 className="mt-4" style={{textAlign: "center"}}>About this site</h1>
-           <Container>
-                <p className="w-50 m-auto">The inspiration of this site was to create a site where everything on it benefits charity, similar to what 
+           <Container className="border rounded-2 mt-2 py-3" style={containerStyle}>
+                <h1 className="mt-2" style={{textAlign: "center"}}>About this site</h1>
+                <p className="w-50 m-auto">This is a site that makes it easier to find items on eBay that benefit charity.
+                    The inspiration of this site was to create a site where everything on it benefits charity, similar to what 
                     Amazon Smile used to be before Amazon ended it. Ebay has a program where any seller can dedicate a portion 
                     of a listing to charity, however in my opinion has no good way to easily find items which benefits charity. 
                     They do have a Charity Shop on their site but there are some problems with it:
                 </p>
 
                 <ul className="w-50 m-auto mt-2">
-                    <li>Its hidden. You have to know where it is to find it.</li>
-                    <li>There is no easy way to do a general item search for things that benefits charity.<br />
-                        For example, I cannot easily do a search for all Nintendo products which benefits charity.</li>
+                    <li>It's hidden. You have to know where it is to find it.</li>
+                    <li>There is no easy way to do a general item search for things that benefit charity.<br />
+                        For example, I cannot easily do a search for all Nintendo products which benefit charity.</li>
                     <li>There is no way to filter search results by a specific charity you want to shop for. <br />
                         "Show me all Nintendo items which benefits the American Cancer Society" cannot be done</li>
                 </ul>
@@ -26,10 +31,10 @@ function AboutPage() {
            </Container>
 
 
-           <Container className="mt-3 mb-5">
+           <Container className="mt-3 mb-5 border rounded-2 py-3" style={containerStyle}>
                 <h1 style={{textAlign: "center"}}>Site Roadmap</h1>
 
-                <p className="w-50 m-auto">Features either curently in progress or will be worked on in the future</p>
+                <p className="w-50 m-auto">Features either curently in progress or will be worked on in the future:</p>
                 <ul className="w-50 m-auto mt-2">
                     <li>The ability to purchase things directly from this site</li>
                     <li>The ability to save your favorite charities</li>
