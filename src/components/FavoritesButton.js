afa806952a471c904108dd6dace279ee8c0a5ffa
anthonycowden <acowden@wgu.edu>
2026-01-12 22:47:38 -0600

    initial commit

---
+++ src/components/FavoritesButton.js
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Image from 'react-bootstrap/Image';
+import StarUnchecked from '../images/starunchecked.png'
+import StarChecked from '../images/starchecked.png'
+import { useDispatch, useSelector } from "react-redux";
+import { addFavorite, removeFavorite } from '../actions/userActions';
+import NormalSpinner from './Spinner';
+
+function FavoritesButton(props) {
+  const favoritesData = useSelector((state) => state.favorites);
+  const { error, loading, favorites } = favoritesData
+  const isFavorite = favorites?.items?.some(item => item.ebay_id === props.id);
+  const [pending, setPending] = useState(false);
+  const dispatch = useDispatch()
+
+  const handleClick = (id) => {
+
+        if (isFavorite){
+            dispatch(removeFavorite(id))
+        }
+        else{
+            dispatch(addFavorite(id))
+        }
+
+        setPending(false)
+    } 
+
+  return (
+    <Button
+  variant="primary"
+  onClick={() => {setPending(true); setTimeout(() => handleClick(props.id), 300)}}
+  disabled={pending}
+  style={{ width: 55, height: 40 }}
+>
+  {pending ? <NormalSpinner /> : <Image src={isFavorite ? StarChecked : StarUnchecked} fluid />}
+     </Button>
+  );
+}
+
+export default FavoritesButton;
