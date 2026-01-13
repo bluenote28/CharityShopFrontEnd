dc6f364c4325a3f03d2fdc1baac88394025778a9
anthonycowden <acowden@wgu.edu>
2026-01-13 07:13:38 -0600

    adds button to run delete items job

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -3,22 +3,30 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getCharities } from '../actions/charityActions';
 import CharitiesTable from '../components/CharitiesTable';
 import SubmitCharityForm from '../components/SubmitCharityForm';
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
+import { Row, Col, Button } from 'react-bootstrap'
 import AdminReport from '../components/AdminReport';
 import NormalSpinner from '../components/Spinner';
 import AlertBox from '../components/Alert';
+import { DatabaseRefreshApi} from '../utilities/ApiClient'
 
 function AdminPage() {
 
    const dispatch = useDispatch();
    const charitiesState = useSelector((state) => state.charities);
    const { error, loading, charities} = charitiesState;
+   const user = useSelector((state) => state.userLogin);
+   const { userInfo } = user
 
     useEffect(() => {
         dispatch(getCharities());
     }, [dispatch])
       
+
+  function deleteItemsFromDB(){
+      const client = new DatabaseRefreshApi(userInfo.access)
+      client.deleteItems()
+      alert("Delete Action Started")
+  }
 
   if (loading){
     return <NormalSpinner />
@@ -37,6 +45,12 @@ function AdminPage() {
                 <Row className='w-50 mt-3 m-auto border rounded-3 px-3'>
                         <h4 className='mt-2' style={{textAlign: 'center'}}>Enter or Update a Charity</h4>
                         <SubmitCharityForm />
+                </Row>
+
+                <Row className='w-50 mt-3 m-auto border rounded-3 px-3'>
+                        <h4 className='mt-2' style={{textAlign: 'center'}}>Database Actions</h4>
+                        <Button onClick={deleteItemsFromDB}>Run Delete Items Job</Button>
+                        
                 </Row>
 
                 <Row className='w-50 mt-3 mb-5 m-auto border rounded-3 px-3'>
