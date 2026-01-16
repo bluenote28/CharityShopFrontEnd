e5e32cf643e20659b60637df689aff16c5c599ed
bluenote28 <136389327+bluenote28@users.noreply.github.com>
2026-01-16 01:37:12 -0600

    adds image carousel and add more items details

--- src/pages/ItemPage.js
+++ src/pages/ItemPage.js
@@ -2,11 +2,11 @@ import { useEffect, useState } from 'react';
 import { BACKEND_API_BASE_URL } from '../constants/apiContants';
 import { useParams } from 'react-router-dom'
 import NormalSpinner from '../components/Spinner';
-import Image from 'react-bootstrap/Image';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import { convertIdToCharityName, covertUrlToAffiliateLink } from '../utilities/Converters';
 import { useSelector, useDispatch } from "react-redux";
 import { getCharities } from '../actions/charityActions';
+import ImageCarousel from '../components/ImageCarousel';
 
 function ItemPage() {
 
@@ -15,6 +15,8 @@ function ItemPage() {
     const charitiesState = useSelector((state) => state.charities);
     const { errorCharities, loadingCharities, charities} = charitiesState;
     const dispatch = useDispatch();
+    const [mainImage, setMainImage] = useState(null)
+    const [altImages, setAltImages ] = useState(null)
 
     useEffect(() => {
         if (!charities || charities.length === 0){
@@ -35,15 +37,19 @@ function ItemPage() {
             setItemData(data)
         }
 
-        fetchItem()
-    }, [item_id])
+        if (!itemData){
+            fetchItem()
+        }
+
+        if (itemData){
+            setMainImage(itemData.img_url)
+            setAltImages(itemData.additional_images.additionalImages)
+        }
+    }, [item_id, itemData])
 
     function handleClick(e, url){
-
         e.preventDefault()
-
         url = covertUrlToAffiliateLink(url);
-
         window.open(url, '_blank');
     }
 
@@ -51,21 +57,31 @@ function ItemPage() {
         return <NormalSpinner />
     }
 
+    console.log(itemData)
+
     return (
         <>
-            <Container className='d-flex flex-column align-items-center mt-4'>
-                <h1>{itemData.name}</h1>
-            </Container>
-
-            <Container className='d-flex flex-column align-items-center mt-4 mb-4'>
-                <Image src={itemData.img_url} style={{maxWidth: '400px'}}/>
-            </Container>
-
-            <Container className='d-flex flex-column align-items-center mb-4'>
-                <h4>Price: ${itemData.price}</h4>
-                <h4>Benefits: {convertIdToCharityName(charities, itemData.charity)}</h4>
-                <Button onClick={(e) => handleClick(e,itemData.web_url)}>Go to item on Ebay</Button>
-            </Container>
+        <Container className='mt-3'>
+            <Row><h2>{itemData.name}</h2></Row>
+            <Row>
+                <Col className='d-flex flex-column align-items-center'>
+                   <Row><ImageCarousel mainImage={mainImage} altImages={altImages} /></Row>
+                </Col>
+                <Col className='d-flex flex-column align-items-center'>
+                    <Container className="border rounded-2 mt-2 p-5" style={{backgroundColor: "#f8f9fa"}}>
+                        <Row><h2 style={{textAlign:"center"}}>Item Details</h2></Row>
+                        <Row><h4>Price: ${itemData.price}</h4></Row>
+                        {itemData.shipping_price ? <Row><h4>Shipping: ${itemData.shipping_price}</h4></Row> : <></>}
+                        {itemData.condition ? <Row><h4>Condition: {itemData.condition}</h4></Row> : <></>}
+                        <Row><h4>Seller: {itemData.seller.username}</h4></Row>
+                        <Row><h4>Total Seller Feedback: {itemData.seller.feedbackScore}</h4></Row>
+                        <Row><h4>Seller Positive Feeback: {itemData.seller.feedbackPercentage}%</h4></Row>
+                        <Row><h4>Benefits: {convertIdToCharityName(charities, itemData.charity)}</h4></Row>
+                        <Row><Button onClick={(e) => handleClick(e,itemData.web_url)}>Go to item on Ebay</Button></Row>
+                    </Container>
+                </Col>
+            </Row>
+        </Container>
         </>
     )   
 }
