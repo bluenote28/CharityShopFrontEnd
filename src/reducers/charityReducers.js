32a9015b5b5cbc6f4b1b953fc6af20cb8ef913b7
anthonycowden <acowden@wgu.edu>
2026-01-21 08:36:00 -0600

    adds update charity

--- src/reducers/charityReducers.js
+++ src/reducers/charityReducers.js
@@ -1,4 +1,7 @@
-import { GET_CHARITIES_REQUEST, GET_CHARITIES_ERROR, GET_CHARITIES_SUCCESS, DELETE_CHARITY_REQUEST, DELETE_CHARITY_ERROR, DELETE_CHARITY_SUCCESS, ADD_CHARITY_ERROR, ADD_CHARITY_REQUEST, ADD_CHARITY_SUCCESS } from '../constants/reducerConstants'
+import { GET_CHARITIES_REQUEST, GET_CHARITIES_ERROR, GET_CHARITIES_SUCCESS, 
+       DELETE_CHARITY_REQUEST, DELETE_CHARITY_ERROR, DELETE_CHARITY_SUCCESS, 
+       ADD_CHARITY_ERROR, ADD_CHARITY_REQUEST, ADD_CHARITY_SUCCESS, UPDATE_CHARITY_ERROR,
+       UPDATE_CHARITY_REQUEST, UPDATE_CHARITY_SUCCESS } from '../constants/reducerConstants'
 
 export const charityReducer = (state = {charities:[]}, action) => {
 
@@ -20,6 +23,12 @@ export const charityReducer = (state = {charities:[]}, action) => {
             case ADD_CHARITY_ERROR:
                 return {loading: false, error: action.error, charities: []}
             case ADD_CHARITY_SUCCESS:
+                return {loading: false, charities: action.payload}
+            case UPDATE_CHARITY_REQUEST:
+                return {loading: true, charities: []}
+            case UPDATE_CHARITY_ERROR:
+                return {loading: false, error: action.error, charities: []}
+            case UPDATE_CHARITY_SUCCESS:
                 return {loading: false, charities: action.payload}
             default:
                 return state
