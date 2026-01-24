5c9ada7063d0c01c4010b1277743641b939e4bd9
bluenote28 <136389327+bluenote28@users.noreply.github.com>
2026-01-24 03:22:55 -0600

    complete redesign

--- src/components/ItemListing.js
+++ src/components/ItemListing.js
@@ -1,83 +1,118 @@
-import { Container, Row, Col, Button, ButtonGroup } from 'react-bootstrap';
-import { useSelector} from "react-redux"
-import Image from 'react-bootstrap/Image';
-import { convertIdToCharityName } from '../utilities/Converters';
-import NormalSpinner from './Spinner';
+import { Container, Row, Col, Button, ButtonGroup } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import Image from "react-bootstrap/Image";
+import NormalSpinner from "./Spinner";
 import { useNavigate } from "react-router-dom";
-import FavoritesButton from './FavoritesButton';
+import FavoritesButton from "./FavoritesButton";
+import { useState } from "react";
 
 const imageStyle = {
-  width: '100%',
-  object: 'cover',
-  height: '315px',
-  cursor: 'pointer'
-}
+  width: "100%",
+  object: "cover",
+  height: "315px",
+  cursor: "pointer",
+};
 
-const bodyStyle = {
-  height: '20rem',
-  padding: '0',
-  border: 'solid'
-}
-
-function ItemListing(props){
-
+function ItemListing(props) {
   const charitiesState = useSelector((state) => state.charities);
-  const { errorCharities, loadingCharities, charities} = charitiesState;
+  const { errorCharities, loadingCharities, charities } = charitiesState;
   const favoritesData = useSelector((state) => state.favorites);
-  const navigate = useNavigate()
+  const navigate = useNavigate();
+  const [charity, SetCharity] = useState(null);
 
-  function handleClick(e, id){
-        e.preventDefault()
-        navigate("/item/" + id)
+  function handleClick(e, id) {
+    e.preventDefault();
+    navigate("/item/" + id);
   }
 
-if (errorCharities){
-  console.log(errorCharities)
-}  
+  function loadCharity() {
+    const charity = charities.find((charity) => charity.id === props.charity);
+    SetCharity(charity);
+  }
 
-else if (loadingCharities){
-    return <NormalSpinner />
-} 
-else if (favoritesData){
-    
-      return (
-      <Container style={bodyStyle}>
-         <Row>   
-            <Col xs={4}>     
-                <Image src={props.image} style={imageStyle} onClick={(e) => handleClick(e,props.id)}/>
-            </Col>         
-            <Col xs={8}>      
-                <Row className="fs-4 fw-bold"><Col style={{cursor: 'pointer'}} onClick={(e) => handleClick(e,props.id)}>{props.title}</Col></Row>
-                <Row className="fs-3"><Col>Price: ${props.price}</Col></Row>
-                <Row className="fs-3"><Col>Benefits: {convertIdToCharityName(charities, props.charity)}</Col></Row>  
-                <Row className='mt-3'>
-                  <ButtonGroup className='w-25' size='sm'>
-                    <FavoritesButton id={props.id}/>
-                  </ButtonGroup>
-                </Row>             
-            </Col>   
-        </Row>
-      </Container>
-      );
-}
-
-else{
-      return (
-       
-        <Container style={bodyStyle}>
-         <Row>   
-            <Col xs={4}>     
-                <Image src={props.image} style={imageStyle} onClick={(e) => handleClick(e,props.id)}/>
-            </Col>
-            <Col xs={8}>      
-                <Row className="fs-4 fw-bold"><Col style={{cursor: 'pointer'}} onClick={(e) => handleClick(e,props.id)}>{props.title}</Col></Row>
-                <Row className="fs-3"><Col>Price: ${props.price}</Col></Row>
-                <Row className="fs-3"><Col>Benefits: {convertIdToCharityName(charities, props.charity)}</Col></Row>
-            </Col>
-        </Row>
-      </Container>
-      );
+  if (errorCharities) {
+    console.log(errorCharities);
+  }
+  else if (loadingCharities) {
+    return <NormalSpinner />;
+  }
+  else if (!favoritesData.error) {
+    if (!charity) {
+      loadCharity();
     }
+
+    return (
+      <Container className="border" style={{ height: "20rem"}}>
+        <Row>
+          <Col xs={4}>
+            <Image
+              src={props.image}
+              style={imageStyle}
+              onClick={(e) => handleClick(e, props.id)}
+            />
+          </Col>
+          <Col>
+            <Row className="fs-5 fw-bold">
+              <Col className="d-flex justify-content-between">
+                <h5 style={{cursor: "pointer"}} onClick={(e) => handleClick(e, props.id)}>{props.title}</h5>
+                <FavoritesButton id={props.id} />
+              </Col>
+            </Row>
+            <Row className="fs-4 mb-3 d-flex justify-content-between">
+              <Col>Price: ${props.price}</Col>
+              <Col className="">
+                <Image
+                  src={charity?.image_url}
+                  style={{width: "300px", height: "200px"}}
+                  />
+              </Col>
+            </Row>
+          </Col> 
+        </Row>
+      </Container>
+    );
+  }
+  else {
+    if (!charity) {
+      loadCharity();
+    }
+
+    return (
+      <Container className="border" style={{ height: "20rem" }}>
+        <Row>
+          <Col xs={4}>
+            <Image
+              src={props.image}
+              style={imageStyle}
+              onClick={(e) => handleClick(e, props.id)}
+            />
+          </Col>
+          <Col>
+            <Row className="fs-5 fw-bold">
+              <Col
+                style={{ cursor: "pointer" }}
+                onClick={(e) => handleClick(e, props.id)}
+              >
+                <h5>{props.title}</h5>
+              </Col>
+            </Row>
+            <Row className="fs-4 mb-2">
+              <Col>Price: ${props.price}</Col>
+            </Row>
+            <Row>
+              <Col className="d-flex flex-row">
+              <Image
+                src={charity?.image_url}
+                style={{width: "300px", height: "200px"}}
+                />
+                <p className="w-75 text-center mx-5">{charity?.description}</p>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
 }
 
 export default ItemListing;
