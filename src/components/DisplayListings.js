a0452909e19171e5668e45a820f1aead4b25b1d4
bluenote28 <136389327+bluenote28@users.noreply.github.com>
2026-01-28 21:41:03 -0600

    rewritten to handle pagination

--- src/components/DisplayListings.js
+++ src/components/DisplayListings.js
@@ -14,28 +14,27 @@ function DisplayListings(props) {
   const [ filteredItems, setFilteredItems ] = useState([])
   const [filteringItems, setFilteringItems ] = useState(true)
   const [page, setPage ] = useState(1)
-  const ITEMS_PER_PAGE = 48;
   const user = useSelector((state) => state.userLogin);
   const { userInfo } = user;
 
 
  const { isPending, isError, data, error } = useQuery({
-    queryKey: [`${[props.search]}${props.subCategory}`],
-    queryFn: () => getItems(null, props.search, props.subCategory),
+    queryKey: [`${[props.search]}${props.subCategory}${page}`],
+    queryFn: () => getItems(null, props.search, props.subCategory, page),
   })
 
   useEffect(() => {
-      setFilteringItems(true)
-    }, [props.charityId, props.subCategory]);
+    setFilteringItems(true)
+    setPage(1)
+  }, [props.charityId, props.subCategory]);
 
   useEffect(() => {
 
       if (isPending || !data) return;
-      if (!Array.isArray(data)) return;
-      const filter = new ListingFilter(data, props.charityId, props.category, props.subCategory);
+      if (!Array.isArray(data.results)) return;
+      const filter = new ListingFilter(data.results, props.charityId, props.category, props.subCategory);
       filter.filterByAll();
       setFilteredItems(filter.getItems());
-      setPage(1);
       setTimeout(()=> setFilteringItems(false), 1);     
     
     }, [data, props.charityId, props.category,props.subCategory, isPending])
@@ -50,10 +49,6 @@ function DisplayListings(props) {
   
   else{
     
-      const totalPages = Math.ceil(filteredItems.length / ITEMS_PER_PAGE);
-      const paginationItemsEnding = ITEMS_PER_PAGE * page;
-      const paginationItemsBeginning = paginationItemsEnding - ITEMS_PER_PAGE;
-      const paginatedItems = filteredItems.slice(paginationItemsBeginning, paginationItemsEnding);
       const prevPaginationItems = [<Pagination.First onClick={() => {setPage(1); window.scrollTo({ top: 0, behavior: 'instant' });}} />, 
       <Pagination.Prev onClick={()=>{
         if(page === 1){
@@ -67,38 +62,32 @@ function DisplayListings(props) {
       
       } />];
       const nextPaginationItems = [<Pagination.Next onClick={()=>{
-
-        if(page === totalPages){
-          return;
-        }
-        else{
-          setPage(page + 1);
-        }
+        setPage(page + 1);
         window.scrollTo({ top: 0, behavior: 'instant' });
-      }
-      }  />, <Pagination.Last onClick={() => {setPage(totalPages); window.scrollTo({ top: 0, behavior: 'instant' });}} />];
+      }} />];
   
       return (
         <>          
         <Container>
             {   
-                paginatedItems.map((item, index) => { 
-
-                    return (
-                    <div key={index}>        
-                        {error ? <p>{error}</p>:
-                          <Row key={index} className='mb-3'>
-                                <ItemListing title={item.name} image={item.img_url} url={item.web_url} id={item.ebay_id} price={item.price} charity={item.charity} />
-                          </Row>
-                         }
-                    </div>
-                    )
-                })
+              data.results.map((item, index) => { 
+                  return (
+                  <div key={index}>        
+                      {error ? <p>{error}</p>:
+                        <Row key={index} className='mb-3'>
+                          <ItemListing title={item.name} image={item.img_url} url={item.web_url} id={item.ebay_id} price={item.price} charity={item.charity} />
+                        </Row>
+                        }
+                  </div>
+                )
+              })
             }
         </Container>
 
         <Container className='d-flex justify-content-center'>
-            <Pagination>{prevPaginationItems}<Pagination.Item>{page}</Pagination.Item><Pagination.Item>of</Pagination.Item><Pagination.Item>{totalPages}</Pagination.Item>{nextPaginationItems}</Pagination>
+            <Pagination>{prevPaginationItems}</Pagination>
+            <div className='d-flex mx-2 mt-1'>Page {page}</div>
+            <Pagination>{nextPaginationItems}</Pagination>
         </Container>
             
         </>  
