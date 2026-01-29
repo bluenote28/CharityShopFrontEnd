f939563cc9d53a4beab98f3d8ea6ae6df5d8bb9a
bluenote28 <136389327+bluenote28@users.noreply.github.com>
2026-01-29 00:28:26 -0600

    removes selects and adds title

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -1,69 +1,18 @@
-import { useState, useEffect } from 'react'
-import Select from 'react-select'
 import DisplayListings from '../components/DisplayListings'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
-import Container from 'react-bootstrap/Container'
-import { useDispatch, useSelector } from 'react-redux';
-import AlertBox from '../components/Alert'
 import { useSearchParams } from 'react-router-dom';
-import { CATEGORY_OPTIONS } from '../constants/categoryFilterOptions'
-import { getCharities } from '../actions/charityActions';
 
 function SearchPage() {
-
-  const [charity, setCharity] = useState(null)
-  const [allCharitites, setAllCharitites] = useState([])
-  const [errorMessage, setErrorMessage ] = useState(null)
+  
   const [searchParams] = useSearchParams();
   const searchText = searchParams.get('search')
-  const [category, setCategory] = useState(searchParams.get('category'))
-  const dispatch = useDispatch();
-  const charitiesState = useSelector((state) => state.charities);
-  const { error, loading, charities} = charitiesState;
-
-  useEffect(() => {
-    if (!charities || charities.length === 0){
-      dispatch(getCharities());
-    }
-  }, [dispatch])
-
-  useEffect(() => {
-    
-          if (charities){
-                  
-              let select_options = [{value: null, label: "All Charities"}];
-
-              for (let i = 0; i < charities.length; i++) {
-                select_options.push({ value: charities[i].id, label: charities[i].name })
-              }              
-              setAllCharitites(select_options);
-            }
-            else{
-              setErrorMessage("Error retrieving charities from server")
-            }
-  }, [charities])
 
   return (
     <>
-    {errorMessage && <AlertBox message={errorMessage}/>}
-  
-      <Row>
-        <Col>
-            
-            <Container className='d-flex justify-content-start mb-3 mt-3'>
-                    
-              <Select className="mx-1 w-25" options={allCharitites} onChange={(e) => setCharity(e.value)} defaultValue={{value: null, label: "All Charities"}} /> 
 
-              <Select className="w-25" options={CATEGORY_OPTIONS} onChange={(e) => setCategory(e.value)} defaultValue={{value: category, label: 'Select Category'}} /> 
-                     
-            </Container>
-        </Col>
-      </Row>
-
-      <DisplayListings charityId={charity} category={category} search={searchText}/>
+    <h1 className='text-center mb-5 mt-3'>Search Results for: {searchText}</h1>
+    <DisplayListings search={searchText} />
        
-      </> 
+    </> 
   )
 }
 
