4ac0855736fc825763a29ddd7ea87fb8f70a1f13
Anthony Cowden <ancowden@gmail.com>
2026-08-31 02:04:21 -0500

    adds purchases link to drop down

--- src/components/Header.js
+++ src/components/Header.js
@@ -41,6 +41,7 @@ function Header() {
                   <NavDropdown title={userInfo.name} id="basic-nav-dropdown">
                     <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>
                     <NavDropdown.Item href="/favorites">Watch List</NavDropdown.Item>
+                    <NavDropdown.Item href="/purchases">Purchases</NavDropdown.Item>
                     <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
                   </NavDropdown>
                   <Nav.Link as={Link} to="/about">About</Nav.Link>
@@ -50,6 +51,7 @@ function Header() {
                   <NavDropdown title={"Welcome"} id="basic-nav-dropdown">
                     <NavDropdown.Item href="/profile">Profile</NavDropdown.Item>
                     <NavDropdown.Item href="/favorites">Watch List</NavDropdown.Item>
+                    <NavDropdown.Item href="/purchases">Purchases</NavDropdown.Item>
                     <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
                   </NavDropdown>
                   <Nav.Link as={Link} to="/about">About</Nav.Link>
