35b6713955e77c594ddfd11c47b2bdaf818da40d
Anthony Cowden <ancowden@gmail.com>
2026-09-07 04:30:26 -0500

    adds about link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,14 @@
 import Image from 'react-bootstrap/Image';
+import { Link } from 'react-router-dom';
 import footerimage from '../images/footerlogo.png'
 
 function Footer(){
 
     return (
 
-        <footer className='d-flex justify-content-center mt-auto' style={{backgroundColor: "#1406d4", width: "100%"}}>
+        <footer className='d-flex flex-column justify-content-center align-items-center mt-auto' style={{backgroundColor: "#1406d4", width: "100%"}}>
            <Image src={footerimage} />
+           <Link to="/about" className="footer-about-link mb-5">About</Link>
         </footer>
     )
 
