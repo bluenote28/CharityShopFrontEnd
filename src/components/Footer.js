ee8b575339d4a4e949e16d23c6804d00ed2b7b37
Anthony Cowden <ancowden@gmail.com>
2026-09-10 09:19:46 -0500

    adds admin link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,22 @@
 import Image from 'react-bootstrap/Image';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import footerimage from '../images/footerlogo.png'
 
 function Footer(){
+    const user = useSelector((state) => state.userLogin);
+    const { userInfo } = user
 
     return (
 
         <footer className='d-flex flex-column justify-content-center align-items-center mt-auto' style={{backgroundColor: "#1406d4", width: "100%"}}>
            <Image src={footerimage} />
-           <Link to="/about" className="footer-about-link mb-5">About</Link>
+           <div className="d-flex flex-row align-items-center gap-3 mb-5">
+             <Link to="/about" className="footer-about-link">About</Link>
+             {userInfo?.isAdmin && (
+               <Link to="/admin" className="footer-about-link">Admin</Link>
+             )}
+           </div>
         </footer>
     )
 
