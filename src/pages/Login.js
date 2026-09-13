47184d6d186504aedfb7b5306edd92acbc28c586
Anthony Cowden <ancowden@gmail.com>
2026-09-12 19:55:06 -0500

    removed login form

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,5 @@
 import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import { Form, Button, Row, Col, Container } from 'react-bootstrap'
-import { useDispatch, useSelector} from "react-redux"
-import { login } from '../actions/userActions'
+import { useSelector} from "react-redux"
 import LoginFormContainer from '../components/LoginFormContainer'
 import FormPageLayout from '../components/FormPageLayout'
 import AlertBox from '../components/Alert'
@@ -10,18 +7,9 @@ import GoogleLoginButton from '../components/GoogleLoginButton'
 
 function LoginPage() {
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('') 
     const userLogin = useSelector(state => state.userLogin)
-    const { loading, error, userInfo } = userLogin
-    const dispatch = useDispatch()
+    const { error, userInfo } = userLogin
     const [alert, setAlert ] = useState('')
-    const RESET_URL = 'https://charity-shop-backend-xm3ai.ondigitalocean.app/password_reset/'
-    
-    const submithandler = (e) => {
-        e.preventDefault()
-        dispatch(login(email, password))
-    }
 
     if (userInfo) {
         window.location.href = '/'
@@ -32,7 +20,7 @@ function LoginPage() {
         if (error){
                     setAlert(error)
                   }
-                
+
         }, [error])
 
 
@@ -41,42 +29,11 @@ function LoginPage() {
             <LoginFormContainer>
                 <h1>Sign In</h1>
                 {alert && <AlertBox message={alert} />}
-                <Form onSubmit={submithandler}>
-                    <Form.Group controlId='email' className='mb-2'>
-                        <Form.Label>Email Address</Form.Label>
-                        <Form.Control className="border-black" type='email' placeholder='Enter email' value={email} onChange={(e) => setEmail(e.target.value)}></Form.Control>
-                    </Form.Group>
-
-                    <Form.Group controlId='password'>
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control className="border-black" type='password' placeholder='Enter password' value={password} onChange={(e) => setPassword(e.target.value)}></Form.Control>
-                    </Form.Group>
-
-                    <Button type='submit' variant='primary'className='mt-2'>
-                        Sign In
-                    </Button>
-                </Form>
-
+                <p>Sign in with your Google account.</p>
                 <GoogleLoginButton />
-
-                <Row className='py-1 mt-2'>
-                    <Col>
-                        <Container className='d-flex'>
-                             <p style={{marginRight: '10px'}}>New Customer?</p> <Link to={'/register'}>Register</Link>
-                        </Container>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Container className='d-flex'>
-                             <p style={{marginRight: '10px'}}>Forgot Password?</p> <Link to={RESET_URL}>Reset Password</Link>
-                        </Container>
-                    </Col>
-                </Row>
-
             </LoginFormContainer>
         </FormPageLayout>
-    )   
+    )
 }
 
 export default LoginPage
