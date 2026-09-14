6145c1f97396797648f75032fa054a129cd183bd
Anthony Cowden <ancowden@gmail.com>
2026-09-14 00:32:15 -0500

    gets rid of unused constant

--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -5,7 +5,6 @@ import { USER_LOGIN_FAIL,
         USER_UPDATE_FAIL,
         USER_UPDATE_REQUEST,
         USER_UPDATE_SUCCESS,
-        USER_UPDATE_PROFILE_RESET,
         GET_FAVORITES_REQUEST,
         GET_FAVORITES_SUCCESS,
         GET_FAVORITES_ERROR,
@@ -43,8 +42,6 @@ export const userUpdateReducer = (state = {}, action) => {
                 return {loading: false, success: true, userInfo: action.payload }
             case USER_UPDATE_FAIL:
                 return {loading: false, error: action.payload}
-            case USER_UPDATE_PROFILE_RESET:
-                return {}
             default:
                 return state
         }
