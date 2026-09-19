303673403cf62b00b69c04525453c683aa367fe8
Anthony Cowden <cowdena@amazon.com>
2026-09-19 00:51:12 -0500

    initial commit

---
+++ src/constants/directoryConstants.js
@@ -0,0 +1,18 @@
+export const DIRECTORY = {
+
+        "Food and Drink" : [
+            {url: "https://www.mysticmonkcoffee.com/", image_url: "https://www.mysticmonkcoffee.com/cdn/shop/files/Web_logo_2_7c9a958a-48df-4880-8b03-87902c2525cb.png?v=1630574429&width=140", name: "Mystic Monk Coffe", description: "Sells coffee and tea to support their monastery"},
+            {url: "https://monasterygreetings.com/", image_url: "https://monasterygreetings.com/cdn/shop/files/Monastery_Greetings_Logo.jpg?v=1736458211&width=600", name: "Monastery Greetings", description: "Sells products from Abbeys, Convents, Hermitages & Monasteries"},
+        ],
+
+        "General Merchandise": [
+            {url: "https://giftshop.stjude.org/index", image_url: "https://giftshop.stjude.org/on/demandware.static/Sites-giftshop-Site/-/default/dwd0f84bb9/images/logo.png", name: "St. Jude Gift Shop", description: "Sells merchandise to support St Jude's Research Hospital"},
+
+        ],
+
+        "Arts and Crafts": [
+            {url: "https://globalgoodspartners.org/", image_url: "https://globalgoodspartners.org/cdn/shop/files/GGP-logo-website.png?v=1738786034&width=300", name: "Global Goods Partners", description: "Sells crafts to support artisans"}
+
+        ]
+
+}
