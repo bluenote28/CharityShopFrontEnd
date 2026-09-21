a4b020a247ace0b406df17ac5b9c0a4878641ac8
Anthony Cowden <ancowden@gmail.com>
2026-09-21 02:12:41 -0500

    changes text

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,5 @@
 import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function AboutPage() {
 
@@ -8,42 +9,16 @@ function AboutPage() {
 
     return (
         <>
-           <Container className="border rounded-2 mt-2 py-3" style={containerStyle}>
-                <h1 className="mt-2" style={{textAlign: "center"}}>About this site</h1>
-                <p className="w-50 m-auto">This is a site that makes it easier to find items on eBay that benefit charity.
-                    The inspiration of this site was to create a site where everything on it benefits charity, similar to what 
-                    Amazon Smile used to be before Amazon ended it. Ebay has a program where any seller can dedicate a portion 
-                    of a listing to charity, however in my opinion has no good way to easily find items which benefits charity. 
-                    They do have a Charity Shop on their site but there are some problems with it:
-                </p>
+           <Container className="border rounded-2 mt-2 mb-5 py-3" style={containerStyle}>
+                <h1 className="mt-2" style={{textAlign: "center"}}>About Charity Shop</h1>
+                <p className="w-50 m-auto">Charity Shop is a website dedicated to helping people find opportunities to make purchases that benefit charity. Every listing you browse here is a chance to buy something you want while supporting a nonprofit at the same time.</p>
+                <p className="w-50 m-auto mt-3">The main part of the site highlights items from the eBay for Charity program. You can search, browse categories, and follow specific charities to discover those listings in one place, then go to eBay to complete the purchase.</p>
+                <p className="w-50 m-auto mt-3">The site also has a <Link to="/directory">directory of other shops</Link> besides eBay where you can make purchases that benefit charity.</p>
 
-                <ul className="w-50 m-auto mt-2">
-                    <li>It's hidden. You have to know where it is to find it.</li>
-                    <li>There is no easy way to do a general item search for things that benefit charity.<br />
-                        For example, I cannot easily do a search for all Nintendo products which benefit charity.</li>
-                    <li>There is no way to filter search results by a specific charity you want to shop for. <br />
-                        "Show me all Nintendo items which benefits the American Cancer Society" cannot be done</li>
-                </ul>
-                
-                <p className="w-50 m-auto mt-3">This site is designed to fix those issues as well as add other features 
-                    (see roadmap).</p>
-                <p className="w-50 m-auto mt-3">If you have any questions or feedback, you can email me at <a href="mailto:ancowden@yahoo.com">ancowden@yahoo.com</a></p>
+                <h2 className="mt-4" style={{textAlign: "center", fontSize: "1.5rem"}}>How eBay for Charity works</h2>
+                <p className="w-50 m-auto">eBay for Charity lets sellers attach a donation to an ordinary eBay listing. When they create the listing, they choose a participating charity and pledge a percentage of the selling price, typically from 10% to 100%. Charity-run shops on eBay donate 100% of the proceeds from their own listings.</p>
+                <p className="w-50 m-auto mt-3">You shop the item the same way you would any other eBay purchase. After the item sells, eBay’s nonprofit partner, PayPal Giving Fund, collects the pledged donation and grants 100% of that amount to the chosen charity. The listing shows which organization benefits and what share of the sale is donated, which is the same information Charity Shop displays when you browse items here.</p>
            </Container>
-
-
-           <Container className="mt-3 mb-5 border rounded-2 py-3" style={containerStyle}>
-                <h1 style={{textAlign: "center"}}>Site Roadmap</h1>
-
-                <p className="w-50 m-auto">Features either curently in progress or will be worked on in the future:</p>
-                <ul className="w-50 m-auto mt-2">
-                    <li>The ability to purchase things directly from this site</li>
-                    <li>The ability to save your favorite charities</li>
-                    <li>The ability for a user to track how much their purchases have benefited charity</li>
-                    <li>A recommmendation system based on the user's purchases and favorites</li>
-                    <li>The ability to host a fundraiser for a particular charity</li>
-                </ul>
-           </Container>
-
         </>
     )   
 }
